a0cd36739c056f4219916f482bf2f1f573f6fc9c
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2022-10-21 21:28:05 +0300

    Enhancements

--- src/test/java/TestCardDeliveryForm.java
+++ src/test/java/TestCardDeliveryForm.java
@@ -8,7 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
-import static org.apache.commons.lang3.StringUtils.*;
 
 public class TestCardDeliveryForm {
   /**
@@ -39,25 +38,21 @@ public class TestCardDeliveryForm {
 
   @Test
   void shouldSuccessfulSentFormWithComplexSelectors(){
+    String city = "Казань";
     int period = 7;
-    String date = generateDate(period, "dd.MM.yyyy");
-    String day = generateDate(period, "dd");
-    String month = generateDate(period, "MM");
-    String currentMonth = generateDate(0, "MM");
-    $("[data-test-id='city'] input").setValue("Ка");
-    $$(".input__popup .menu-item_type_block").findBy(exactText("Казань")).click();
+    int defaultPeriod = 3;
+    $("[data-test-id='city'] input").setValue(city.substring(0, 2));
+    $$(".input__popup .menu-item_type_block").findBy(text("Казань")).click();
     $("[data-test-id='date']").click();
-    if (Integer.parseInt(month) > Integer.parseInt(currentMonth)){
-      $$(".calendar__arrow_direction_right").findBy(attribute("data-step","1")).click();
-      $$(".calendar__day" ).find(exactText(stripStart(day,"0"))).click();
-    } else {
-    $$(".calendar__day" ).find(exactText(stripStart(day,"0"))).click();
+    if (!generateDate(period, "MM").equals(generateDate(defaultPeriod, "MM"))){
+      $("[data-step='1']").click();
     }
+    $$(".calendar__day" ).find(text(generateDate(period,"d"))).click();
     $("[data-test-id='name'] input").setValue("Василий Бонч-Бруевич");
     $("[data-test-id='phone'] input").setValue("+79269998888");
     $("[data-test-id='agreement']").click();
     $$("button").find(exactText("Забронировать")).click();
     $("[data-test-id='notification']  .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(exactText("Успешно!"));
-    $("[data-test-id='notification']  .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована на " + date));
+    $("[data-test-id='notification']  .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована на " + generateDate(period, "dd.MM.yyyy")));
   }
 }
